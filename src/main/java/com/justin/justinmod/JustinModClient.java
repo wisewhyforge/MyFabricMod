966319e96687902d0087a0bb5201c609c135e16a
Justin Blacsza <duitnn@gmail.com>
2024-12-13 08:43:54 -0500

    Initial Commit

---
+++ src/main/java/com/justin/justinmod/JustinModClient.java
@@ -0,0 +1,11 @@
+package com.justin.justinmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JustinModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
